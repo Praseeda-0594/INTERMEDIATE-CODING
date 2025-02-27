50af3c13e3ced37dcc30bde4b316524e239d098d
Praseeda-0594 <satluri3@gitam.in>
2025-02-27 14:37:54 +0530

    Update week11:combsum.py

--- week11:combsum.py
+++ week11:combsum.py
@@ -1,5 +1,4 @@
-# Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
-
+# Find all valid combinations of k numbers that sum up to n such that the following conditions are true: Only numbers 1 through 9 are used. Each number is used at most once.
 from itertools import combinations
 
 class Solution(object):
